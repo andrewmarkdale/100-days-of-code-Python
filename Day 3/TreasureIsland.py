74693b5148bd50973edbf92a84b2a379497f8ff2
andrewmarkdale <andrew.dale@ontariotechu.net>
2023-03-18 00:40:54 -0400

    Finished Day 3, I extended it quite a bit

--- Day 3/TreasureIsland.py
+++ Day 3/TreasureIsland.py
@@ -65,33 +65,26 @@ ring = False
 first_choice = random.choice(left_right)
 second_choice = random.choice(forward_left_right)
 third_choice = random.choice(colour_choice)
-fourth_choice = random.choice(forward_left_right)
-fifth_choice = random.choice(left_right)
+
 
 
 # To give player hints for second choice
 second_choice_wrong = [x for x in forward_left_right if x != second_choice]
 second_choice_wrong1 = random.choice(second_choice_wrong)
-second_choice_wrong2 = second_choice_wrong.remove(second_choice_wrong1)
-
-
-# To give player hints for fourth choice
-fourth_choice_wrong = [x for x in forward_left_right if x != fourth_choice]
-fourth_choice_wrong1 = random.choice(fourth_choice_wrong)
-
-fifth_choice_wrong = left_right[:]
-fifth_choice_wrong.remove(fifth_choice)
+second_choice_wrong2 = second_choice_wrong
+# Feels hacky but I was getting sick of this project
+second_choice_wrong2.remove(second_choice_wrong1)
 
 print("You awake on a stormy, windy morning. Waves are crashing on the beach not " + 
       "50 yards from the tavern in which you've just spent the night. Your head " +
-      "throbs from the previous night of debauchery. Before your journey into the " +
+      "throbs from the previous night of debauchery before your journey into the " +
       "jungle in search of Jack Longbeard's treasure.")
 
 print("\nOutside you are approached by a weathered pirate. He asks if you're willing to listen to \
 his tale.")
 
 
-tale = input("\nWill you listen to the pirates tale? Y\\N ")
+tale = input("\nWill you listen to the pirates tale? Y/N ")
 
 if tale.lower() == "y":
     
@@ -100,7 +93,7 @@ if tale.lower() == "y":
           "fraught wit' danger, traps around every corner. I 'ave made it only part o' the way thar. I " +
           "'ave noted me journey this far. Here, fer ye fer indulgin' an ole pirate.")
     
-    print(f"\nThe pirate hands you a piece of paper with his notes. It reads, \n " +
+    print("\nThe pirate hands you a piece of paper with his notes. It reads, \n " +
           f"Thar are no monsters t' the {first_choice}. At the next crossroads I 'ave lost two " + 
           f"scallywags venturin' t' the way {second_choice_wrong1}.")
     
@@ -108,56 +101,100 @@ if tale.lower() == "y":
     
     cow_return = input("\nI merely 'ave a request. Me cap'n once nattered about this golden cow " + 
                        "statue looted from his village as a sprog. It may 'ave some monetary value but 'tis " + 
-                       "o' more sentimental value t' me. If ye find it, would ye kindly return it? Y\\N ")
+                       "o' more sentimental value t' me. If ye find it, would ye kindly return it? Y/N ")
     
     if cow_return.lower() == "y":
-        print("I thank ye, take this on yer journey. 'tis me lucky ring, I found it " + 
+        print("\nI thank ye, take this on yer journey. 'tis me lucky ring, I found it " + 
               "leadin' t' ole Longbeards booty. May it serve ye well.")
-        print(f"The old pirate hands you a {third_choice} gemmed ring. You feel a " + 
+        print(f"\nThe old pirate hands you a {third_choice} gemmed ring. You feel a " + 
                "little more confident upon donning the ring.")
-        input("Enter any key to continue ")
+        input("\nEnter any key to continue ")
+        ring = True
     else:
-        print("'Tis too bad, I wish ye luck on yer journey.")
+        print("\n'Tis too bad, I wish ye luck on yer journey.")
 else:
-    print("\"A pox upon ye\", the pirate says in a low, gruff voice.")
+    print("\n\"A pox upon ye\", the pirate says in a low, gruff voice.")
     
 
 # First fork in the road
 print("\nThe journey begins. The jungle is perilous, filled with snakes and jaguars. " +
 "A number of members from your small team have already been lost when you finally come " + 
 "upon the first fork in the road. The road forks left and right.")
-choice = input("What way will you journey? left/right ")
+
 while True: 
+    choice = input("\nWhat way will you journey? left/right ")
     if choice.lower() != first_choice:
-        print(f"You decide to take the {choice} path. Before you know it you're " +
+        print(f"\nYou decide to take the {choice} path. Before you know it you're " +
                "sinking in quicksand.")
         if not ring:
-            print("You cannot find a way out of the quicksand. You have perished.")
+            print("\nYou cannot find a way out of the quicksand. You have perished.")
+            print("\nGame over")
             quit()
         else:
-            print("You find a rope to pull yourself out. The old pirate's lucky " +
+            print("\nYou find a rope to pull yourself out. The old pirate's lucky " +
                   "ring seems to dissapate before you backtrack and choose the correct path.")
             ring = False
+            break 
     elif choice.lower() == first_choice: 
-        print("You seem to have chosen correctly. The path, while initially " +
-    "overgrown, quickly opens into a well-groomed path.")
+        print("\nYou seem to have chosen correctly. The path, while initially " +
+    "overgrown, quickly opens into a green expanse with a clearly defined path.")
+        break
     else:
+        print("\nPlease choose one of the available options\n")
         
 # Second three way fork
 
-print(f"After travelling on the path for a few hours you finally come upon another choice.\
-The path goes in three directions: left, right and forward. You suddenly see a flock of\
-birds fly away in the {second_choice_wrong2} direction.")
-choice = input("What way do you choose? L/R/F ")
-if choice != second_choice:
-    if not ring:
-        print(f"You march on in the {choice} direction. You come upon beasts. Without\
-a way to defend yourself you perish.")
-        quit() 
-    else:
-        print(f"You march on in the {choice} direction. You come upon beasts. Luckily, you find\
-a sword to defend yourself. Once you've slain the beasts you backtrack and march down the\
-correct direction. The old pirate's ring seems to dissapate before your very eyes.")
-        
-
+print("\nAfter travelling on the path for a few hours you finally come upon another choice. " +
+"The path goes in three directions: left, right and forward. You suddenly see a flock of " +
+f"birds fly away in the {second_choice_wrong2[0]} direction.")
+while True:
+    choice = input("\nWhat way do you choose? left/right/forward ")
+    if choice.lower() != second_choice:
+        if not ring:
+            print(f"\nYou march on in the {choice} direction. You come upon beasts. Without " +
+                   "a way to defend yourself you perish.")
+            print("\nGame over")
+            quit() 
+        else:
+            print(f"\nYou march on in the {choice} direction. You come upon beasts. Luckily, you find " +
+                   "a sword to defend yourself. Once you've slain the beasts you backtrack and march down the " +
+                   "correct direction. The old pirate's ring seems to dissapate before your very eyes.")
+            break
+    elif choice.lower() == second_choice:
+        print(f"\nYou proceed down the {choice} path. You seem to have chosen correctly. " +
+               "The path leads into a cave, you hesistantly proceed.")
+        input("\nPress enter to continue ")
+        break
     
+print("\nThe cave, while initially scary, seems easy to traverse. After travelling a while " +
+      "you have come upon a series of doors lit by torches. \"Lit torches in a cave, interesting\", you say aloud.")
+
+if ring:
+    print(f"\nYou look down at your hand and notice the ring dissapating. Before it ceases to exist fully it releases a {third_choice} flash.")
+print("\nLooking at the doors, each seems to be marked by a specific colour. The colours marking the doors are " +
+      ", ".join(colour_choice[:-1]) + " and " + colour_choice[-1])
+            
+choice = input("\nWhat door will you choose: " + ", ".join(colour_choice[:-1]) + " or " + colour_choice[-1] + "? ") 
+choice = choice.lower()
+if third_choice != choice:
+    if choice == "red":
+        print("\nYou walk through the door marked by red. All of a sudden the floor collapses and you fall into a pit of fire and perish.")
+        print("\nGame over")
+        quit()
+    elif choice == "purple":
+        print("\nYou walk through the door marked by purple. The walls start releasing mist. Before you know it " +
+              "you feel yourself turning to crystal. You struggle, but it is ultimately fruitless. You perish.")
+        print("\nGame over")
+        quit()
+    elif choice == "blue":
+        print("\nYou walk through the door marked by blue. The floor gives way and you plunge into water in a dark cavern. " +
+              "You can seem to regain your footing and ultimately drown.")
+        print("\nGame over")
+        quit()
+    else:
+        print(f"You walk through the door marked by {choice}. Before you know it, you are swarmed by beasts. You perish.")
+        print("\nGame over")
+        quit()
+else: 
+    print("\nYou seem to have chosen correctly. After winding through a long, twisting hallway you come upon the treasure!")
+    print("\nYou win!")       
