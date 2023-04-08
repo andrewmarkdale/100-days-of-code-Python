babbb8294eebaf0c900d35984151609b55a92526
andrewmarkdale <andrew.dale@ontariotechu.net>
2023-04-08 17:23:11 -0400

    Challenge 3

---
+++ Day018/turtle_challenge3.py
@@ -0,0 +1,18 @@
+# Draw different shapes
+
+from turtle import Turtle, Screen
+
+screen = Screen()
+timmy = Turtle()
+
+def draw_shapes(turtle_obj, list_of_sides):
+    for s in list_of_sides:
+        side_length = s * 10
+        angle = 360/s
+        for _ in range(s):
+            turtle_obj.forward(side_length)
+            turtle_obj.left(angle)
+            
+sides = [x for x in range(3, 11)]
+draw_shapes(timmy, sides)
+screen.exitonclick()
