dd291506ca9d49fa13d8248610c837671cdf4b0b
andrewmarkdale <andrew.dale@ontariotechu.net>
2023-04-09 05:40:59 -0400

    Etch-A-Sketch

---
+++ Day019/etchasketch.py
@@ -0,0 +1,33 @@
+
+import turtle as t
+import random
+
+timmy = t.Turtle()
+screen = t.Screen()
+
+def move_forward():
+    timmy.forward(10)
+    
+def move_backward():
+    timmy.backward(10)
+    
+def turn_right():
+    timmy.right(5)
+    
+def turn_left():
+    timmy.left(5)
+    
+def clear_screen():
+    timmy.clear()
+    
+def reset_screen():
+    timmy.reset()
+
+screen.onkey(key="Up", fun=move_forward)
+screen.onkey(key="Right", fun=turn_right)
+screen.onkey(key="Left", fun=turn_left)
+screen.onkey(key="Down", fun=move_backward)
+screen.onkey(key="c", fun=clear_screen)
+screen.onkey(key="r", fun=reset_screen)
+t.listen()
+screen.exitonclick()
